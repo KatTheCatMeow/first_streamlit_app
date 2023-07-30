7d1a46e02fceea69b0c6184dd5a5dbc5e9dec1c5
KatTheCatMeow <82709869+KatTheCatMeow@users.noreply.github.com>
2023-07-31 03:06:28 +0800

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry OatmeaL")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry OatmeaL')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
